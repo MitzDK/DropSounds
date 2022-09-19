a313fc967dbf427c611709a1c2c993071582b309
MitzDK <victorwilliamjejlskov@gmail.com>
2022-09-19 16:25:52 +0200

    Update SoundFileManager.java

--- src/main/java/com/github/MitzDK/SoundFileManager.java
+++ src/main/java/com/github/MitzDK/SoundFileManager.java
@@ -27,7 +27,7 @@ public abstract class SoundFileManager {
     private static final File DOWNLOAD_DIR = new File(RuneLite.RUNELITE_DIR.getPath() + File.separator + "drop-sounds");
     private static final String DELETE_WARNING_FILENAME = "EXTRA_FILES_WILL_BE_DELETED_BUT_FOLDERS_WILL_REMAIN";
     private static final File DELETE_WARNING_FILE = new File(DOWNLOAD_DIR, DELETE_WARNING_FILENAME);
-    private static final HttpUrl RAW_GITHUB = HttpUrl.parse("https://raw.githubusercontent.com/m0bilebtw/c-engineer-completed/sounds");
+    private static final HttpUrl RAW_GITHUB = HttpUrl.parse("https://raw.githubusercontent.com/MitzDK/DropSounds/sounds");
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void ensureDownloadDirectoryExists() {
